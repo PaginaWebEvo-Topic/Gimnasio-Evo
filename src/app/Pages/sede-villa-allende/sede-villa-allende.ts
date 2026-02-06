a5ec8ae9055dbc783f8c20b91e059be593de2ecb
andresbianchiispc <andresbianchi.ok@gmail.com>
2026-02-06 17:19:13 -0300

    villa allende

---
+++ src/app/Pages/sede-villa-allende/sede-villa-allende.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sede-villa-allende',
+  imports: [],
+  templateUrl: './sede-villa-allende.html',
+  styleUrl: './sede-villa-allende.css',
+})
+export class SedeVillaAllende {
+
+}
