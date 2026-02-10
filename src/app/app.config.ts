4b6cdf9cc8b7a223f6e2a3f0de2c9710e210f4bc
andresbianchiispc <andresbianchi.ok@gmail.com>
2026-02-09 22:26:54 -0300

    feat: add server-side rendering support and update dependencies
    - Updated package.json to include SSR commands and dependencies.
    - Modified app.config.ts to provide client hydration.
    - Updated tsconfig.app.json to include Node types.
    - Added routes.txt for application routes.
    - Created app.config.server.ts for server-side rendering configuration.
    - Added app.routes.server.ts to define server routes for prerendering.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -9,6 +10,6 @@ export const appConfig: ApplicationConfig = {
       withInMemoryScrolling({ scrollPositionRestoration: 'enabled',
         anchorScrolling: 'enabled',
        })
-    )
+    ), provideClientHydration(withEventReplay())
   ]
 };
